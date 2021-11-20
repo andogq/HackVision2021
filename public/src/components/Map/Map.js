099ec69b6b01d5db0484f46f72b1ad850ccadd04
Justin Naismith <justin.n@live.com.au>
2021-11-20 12:35:52 +1100

    Updated map pins

--- public/src/components/Map/Map.js
+++ public/src/components/Map/Map.js
@@ -1,18 +1,26 @@
-import GoogleMapReact from "google-map-react"
 import Pin from "./Pin"
+
+import GoogleMapReact from "google-map-react"
+// https://www.npmjs.com/package/google-map-react
 
 const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY
 
-const Map = ({ location, zoom }) => (
+const Map = ({ defaultCenter, zoom, pins }) => (
   <div style={{ height: "100vh", width: "100%" }}>
     <GoogleMapReact
       bootstrapURLKeys={{ key: API_KEY }}
-      defaultCenter={location}
+      defaultCenter={defaultCenter}
       defaultZoom={zoom}
     >
-      <Pin lat={location.lat} lng={location.lng} text={location.address} />
+      <Pins pins={pins} />
     </GoogleMapReact>
   </div>
 )
+
+const Pins = ({ pins }) => {
+  return pins
+    ? pins.map((pin) => <Pin lat={pin.lat} lng={pin.lng} text={pin.address} />)
+    : null
+}
 
 export default Map
--- public/src/pages/Home/Home.js
+++ public/src/pages/Home/Home.js
@@ -1,17 +1,32 @@
 import { Navigation, Map } from "components"
 
-const location = {
-  address: "1600 Amphitheatre Parkway, Mountain View, california.",
-  lat: 37.42216,
-  lng: -122.08427,
+const defaultCenter = {
+  name: "RMIT Building 80",
+  address: "445 Swanston Street Melbourne",
+  lat: -37.80811940260482,
+  lng: 144.96263556935406,
 }
+const pins = [
+  {
+    name: "Darebin Resource Recovery Center",
+    address: "Kurnai Avenue, Reservoir VIC 3073",
+    lat: -37.715784512691286,
+    lng: 144.98359122149628,
+  },
+  {
+    name: "Moonee Valley Transfer Station",
+    address: "188 Holmes Rd, Aberfeldie VIC 3040",
+    lat: -37.75813999854208,
+    lng: 144.90188040581572,
+  },
+]
 
 const Home = () => (
   <div>
     <h1>Home page</h1>
 
     <Navigation />
-    <Map location={location} zoom={4} />
+    <Map defaultCenter={defaultCenter} zoom={14} pins={pins} />
   </div>
 )
 
