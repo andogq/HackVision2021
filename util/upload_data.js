5c547346af5d91e51a5cde623c050c8a5844e077
Justin Naismith <justin.n@live.com.au>
2021-11-21 02:10:14 +1100

    added extra images

--- util/upload_data.js
+++ util/upload_data.js
@@ -2,17 +2,17 @@ const { initializeApp, applicationDefault, cert } = require("firebase-admin/app"
 const { getFirestore, Timestamp, FieldValue } = require("firebase-admin/firestore");
 
 const serviceAccount = require("../service_account.json");
-const data = require("../data/sites.json");
+const data = require('../data/posts.json')
 
 async function run() {
-    initializeApp({ credential: cert(serviceAccount) });
-    const db = getFirestore();
+  initializeApp({ credential: cert(serviceAccount) })
+  const db = getFirestore()
 
-    const collection = db.collection("sites");
+  const collection = db.collection('posts')
 
-    await Promise.all(data.map(site => collection.add(site)));
+  await Promise.all(data.map((post) => collection.add(post)))
 
-    console.log("Finished uploading data");
+  console.log('Finished uploading data')
 }
 
 run();
